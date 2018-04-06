59a1aca530175ff02f631285e0776e6386f4dcec
Michael <michael.bailey788@topper.wku.edu>
2018-04-06 14:09:15 -0500

    Created exercise MC

--- db/models/exercise.js
+++ db/models/exercise.js
@@ -1,16 +1,15 @@
-'use strict';
 module.exports = (sequelize, DataTypes) => {
-  var Exercise = sequelize.define('Exercise', {
+  const Exercise = sequelize.define('Exercise', {
     distance: {
       type: DataTypes.DOUBLE,
-      allowNull: false
+      allowNull: false,
     },
     method: {
       type: DataTypes.ENUM('W', 'R', 'C'),
       allowNull: false,
     },
   }, {});
-  Exercise.associate = function(models) {
+  Exercise.associate = function (models) {
     Exercise.belongsTo(models.User, {
       foreignKey: 'userID',
     });
